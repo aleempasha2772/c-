f1dc5a91c21320bc8645ad305fa696bc6ae83649
aleempasha2772 <aleempasha1410@gmail.com>
2025-05-01 16:28:18 +0530

    Creating a server from scratch using c

---
+++ server.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <fcntl.h> // For O_RDONLY
+#include <sys/stat.h> // For stat()
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+#define DOCUMENT_ROOT "./www" // Directory to serve files from
+
+int main(){
+    printf("Hello World\n");
+    int server_fd, client_socket;
+    struct sockaddr_in server_addr,client_addr;
+    socklen_t client_addr_len = sizeof(client_addr);
+    char buffer[BUFFER_SIZE];
+    char method[16],uri[256],version[16]; // to store parts of the request line
+    char file_path[512];
+
+/*
+server_fd : server file discriptor
+socket() creates endpoint for communicaion always three integer arguments;
+AF_NET : Address Family - Internet , this indicates socket will use IPV4 protocol
+SOCK_STREAM : stream socket 
+protocol: 0 , AF_INET and SOCK_STREAM, the default and standard protocol is TCP.
+int opt = 1; : This line declares an integer variable opt and sets its value to 1
+setsockopt(...): This is the system call used to manipulate options for the socket.
+SOL_SOCKET means the option is at the general socket layer itself, not specific to a particular protocol like TCP or IP.
+
+
+*/
+
+    server_fd = socket(AF_INET,SOCK_STREAM,0);
+    if (server_fd < 0) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+    return 0;
+}
